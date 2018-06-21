92c8ab1a6cd83a6f7ca833d7e0bde7a83198c0d7
Nemanja Radosavljevic <nradosavljevic@itekako.com>
2018-06-21 12:10:12 +0200

    uncomment buildNewMdeState

--- src/util/DraftUtil.ts
+++ src/util/DraftUtil.ts
@@ -1,13 +1,15 @@
-import {EditorState, ContentState, SelectionState} from "draft-js";
-import {MarkdownState} from "../types/MarkdownState";
-import {GenerateMarkdownPreview, MdeState, TextSelection} from "../types";
+import { EditorState, ContentState, SelectionState } from 'draft-js';
+import { MarkdownState } from '../types/MarkdownState';
+import { GenerateMarkdownPreview, MdeState, TextSelection } from '../types';
 
 export function getContentLengthOfAllBlocksBefore(editorState, key) {
     let count = 0;
     let blockBefore;
     let currentKey = key;
     while (true) {
-        blockBefore = editorState.getCurrentContent().getBlockBefore(currentKey);
+        blockBefore = editorState
+            .getCurrentContent()
+            .getBlockBefore(currentKey);
         if (!blockBefore) {
             break;
         }
@@ -26,17 +28,39 @@ export function getSelection(editorState): TextSelection {
     const endKey = selection.getEndKey();
     const endOffset = selection.getEndOffset();
 
-    const editorWiseOffset = getContentLengthOfAllBlocksBefore(editorState, startKey);
-    const offsetBetweenKeys = getContentLengthBetween(editorState, startKey, startOffset, endKey, endOffset);
+    const editorWiseOffset = getContentLengthOfAllBlocksBefore(
+        editorState,
+        startKey,
+    );
+    const offsetBetweenKeys = getContentLengthBetween(
+        editorState,
+        startKey,
+        startOffset,
+        endKey,
+        endOffset,
+    );
     // start and end are on the same block
-    return {start: startOffset + editorWiseOffset, end: startOffset + offsetBetweenKeys + editorWiseOffset};
+    return {
+        start: startOffset + editorWiseOffset,
+        end: startOffset + offsetBetweenKeys + editorWiseOffset,
+    };
 }
 
-export function getContentLengthBetween(editorState, startKey, startOffset, endKey, endOffset) {
+export function getContentLengthBetween(
+    editorState,
+    startKey,
+    startOffset,
+    endKey,
+    endOffset,
+) {
     if (startKey === endKey) {
         return endOffset - startOffset;
     }
-    let count = editorState.getCurrentContent().getBlockForKey(startKey).getText().length - startOffset;
+    let count =
+        editorState
+            .getCurrentContent()
+            .getBlockForKey(startKey)
+            .getText().length - startOffset;
     let blockAfter;
     let currentKey = startKey;
     while (true) {
@@ -45,7 +69,7 @@ export function getContentLengthBetween(editorState, startKey, startOffset, endK
             break;
         }
         // we have to add 1 here to account for the \n character
-        count += (blockAfter.getText().length + 1);
+        count += blockAfter.getText().length + 1;
         currentKey = blockAfter.getKey();
     }
     // we have to add 1 here to account for the \n character
@@ -54,16 +78,28 @@ export function getContentLengthBetween(editorState, startKey, startOffset, endK
 }
 
 export function getPlainText(editorState: EditorState): string {
-    return editorState.getCurrentContent().getPlainText("\n");
+    return editorState.getCurrentContent().getPlainText('\n');
 }
 
-const findBlockKeyAndOffsetForPosition = (position, block, globalOffset, blockOffset, contentState) => {
+const findBlockKeyAndOffsetForPosition = (
+    position,
+    block,
+    globalOffset,
+    blockOffset,
+    contentState,
+) => {
     if (!block || position < globalOffset + blockOffset) {
         return null;
     }
     if (position > globalOffset + block.getText().length) {
         // the princess is in another castle
-        return findBlockKeyAndOffsetForPosition(position, contentState.getBlockAfter(block.getKey()), globalOffset + block.getText().length + 1, 0, contentState);
+        return findBlockKeyAndOffsetForPosition(
+            position,
+            contentState.getBlockAfter(block.getKey()),
+            globalOffset + block.getText().length + 1,
+            0,
+            contentState,
+        );
     } else {
         // the princess is in this castle
         return {
@@ -74,20 +110,37 @@ const findBlockKeyAndOffsetForPosition = (position, block, globalOffset, blockOf
     }
 };
 
-export function buildSelectionState(contentState: ContentState, selection: TextSelection) {
+export function buildSelectionState(
+    contentState: ContentState,
+    selection: TextSelection,
+) {
     const firstBlock = contentState.getFirstBlock();
     if (firstBlock === null) {
         return null;
     }
-    const startBlockData = findBlockKeyAndOffsetForPosition(selection.start, firstBlock, 0, 0, contentState);
+    const startBlockData = findBlockKeyAndOffsetForPosition(
+        selection.start,
+        firstBlock,
+        0,
+        0,
+        contentState,
+    );
     if (startBlockData === null) {
         return null;
     }
-    const endBlockData = findBlockKeyAndOffsetForPosition(selection.end, startBlockData.block, startBlockData.globalOffset, startBlockData.blockOffset, contentState);
+    const endBlockData = findBlockKeyAndOffsetForPosition(
+        selection.end,
+        startBlockData.block,
+        startBlockData.globalOffset,
+        startBlockData.blockOffset,
+        contentState,
+    );
     if (endBlockData === null) {
         return null;
     }
-    const selectionState = SelectionState.createEmpty(startBlockData.block.getKey());
+    const selectionState = SelectionState.createEmpty(
+        startBlockData.block.getKey(),
+    );
     return selectionState.merge({
         anchorKey: startBlockData.block.getKey(),
         anchorOffset: startBlockData.blockOffset,
@@ -96,16 +149,23 @@ export function buildSelectionState(contentState: ContentState, selection: TextS
     }) as SelectionState;
 }
 
-export function getMarkdownStateFromDraftState(editorState: EditorState): MarkdownState {
+export function getMarkdownStateFromDraftState(
+    editorState: EditorState,
+): MarkdownState {
     return {
         text: getPlainText(editorState),
         selection: getSelection(editorState),
     };
 }
 
-export async function getMdeStateFromDraftState(editorState: EditorState, generateMarkdownPreview: GenerateMarkdownPreview): Promise<MdeState> {
+export async function getMdeStateFromDraftState(
+    editorState: EditorState,
+    generateMarkdownPreview: GenerateMarkdownPreview,
+): Promise<MdeState> {
     const markdown = getPlainText(editorState);
-    const html = generateMarkdownPreview ? await generateMarkdownPreview(markdown) : "";
+    const html = generateMarkdownPreview
+        ? await generateMarkdownPreview(markdown)
+        : '';
     return {
         html,
         markdown,
@@ -113,14 +173,20 @@ export async function getMdeStateFromDraftState(editorState: EditorState, genera
     };
 }
 
-export function buildNewDraftState(currentState: EditorState, markdownState: MarkdownState) {
-    const {text, selection} = markdownState;
+export function buildNewDraftState(
+    currentState: EditorState,
+    markdownState: MarkdownState,
+) {
+    const { text, selection } = markdownState;
     // TODO: Fix the redo. It's no working properly but this is an implementation detail.
 
     // handling text change history push
     const contentState = ContentState.createFromText(text);
-    let state = EditorState.forceSelection(currentState, currentState.getSelection());
-    state = EditorState.push(state, contentState, "insert-characters");
+    let state = EditorState.forceSelection(
+        currentState,
+        currentState.getSelection(),
+    );
+    state = EditorState.push(state, contentState, 'insert-characters');
 
     // handling text selection history push
     const selectionState = selection
@@ -130,14 +196,23 @@ export function buildNewDraftState(currentState: EditorState, markdownState: Mar
     return EditorState.forceSelection(state, selectionState);
 }
 
-/*
-export async function buildNewMdeState(currentState: MdeState, generateMarkdownPreview: GenerateMarkdownPreview, newText: string, newSelection: TextSelection = null): Promise<MdeState> {
-    const newDraftState = buildNewDraftState(currentState.draftEditorState, newText, newSelection);
-    const html = generateMarkdownPreview ? await generateMarkdownPreview(newText) : "";
+export async function buildNewMdeState(
+    currentState: MdeState,
+    generateMarkdownPreview: GenerateMarkdownPreview,
+    newText: string,
+    newSelection: TextSelection = null,
+): Promise<MdeState> {
+    const newDraftState = buildNewDraftState(
+        currentState.draftEditorState,
+        newText,
+        newSelection,
+    );
+    const html = generateMarkdownPreview
+        ? await generateMarkdownPreview(newText)
+        : '';
     return {
         html,
         markdown: newText,
         draftEditorState: newDraftState,
     };
 }
-*/
